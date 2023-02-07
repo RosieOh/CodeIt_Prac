d9b1fa19562ffb038eb3b48e55676e7725ca7f57
RosieOh <20172207@gm.hannam.ac.kr>
2023-02-07 12:55:15 +0900

    코드잇 예제 실습

---
+++ mindnote/diary/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('diary/info/', include('views.info')),
+    # path('diary/write/', include('views.page_create')),
+    # path('diary/page/<int:page_id>/', include('views.page_detail')),
+    # path('diary/page/<int:page_id>/edit/', include('views.page_list')),
+    # path('diary/page/<int:page_id>/delete/', include('views.page_delete')),
+]
